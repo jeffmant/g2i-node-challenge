9fc77c848d941bb3080077543390b364578293c6
jeffmant <jgsmantovani@gmail.com>
2022-05-24 19:18:06 -0300

    feat: ensure acronym entity returns created entity when all params are provided

--- src/domain/entity/acronym.entity.spec.ts
+++ src/domain/entity/acronym.entity.spec.ts
@@ -14,4 +14,11 @@ describe('Acronym Entity Unit Tests', () => {
       expect(sut).toBeUndefined()
     }).toThrowError(new Error('Must have definition as string'))
   })
+
+  it('Should returns a created acronym entity when all params are provided', () => {
+    const sut = new Acronym('ANY_ACRONYM', 'Any Acronym')
+    expect(sut).toBeDefined()
+    expect({ title: sut.title, definition: sut.definition })
+      .toEqual({ title: 'ANY_ACRONYM', definition: 'Any Acronym' })
+  })
 })
