47290ff288e763c7acc2ef93f4da5a5bcb888a68
jeffmant <jgsmantovani@gmail.com>
2022-05-27 11:36:50 -0300

    test: ensure acronym repository throws when try to create a duplicated one

--- src/infra/repository/acronym.repository.ts
+++ src/infra/repository/acronym.repository.ts
@@ -1,9 +1,22 @@
 import { Acronym } from '../../domain/entity/acronym.entity'
 import { IRepository } from '../../domain/repository/repository.interface'
 import { AcronymModel } from '../database/sequelize/model/acronym.model'
-
+import { Op } from 'sequelize'
 export class AcronymRepository implements IRepository<Acronym> {
   async create (acronymData: Acronym): Promise<void> {
+    const foundAcronym = await AcronymModel.findOne({
+      where: {
+        [Op.or]: [
+          { title: acronymData.title },
+          { definition: acronymData.definition }
+        ]
+      }
+    })
+
+    if (foundAcronym) {
+      throw new Error('Acronym already exists')
+    }
+
     await AcronymModel.create({
       title: acronymData.title,
       definition: acronymData.definition
