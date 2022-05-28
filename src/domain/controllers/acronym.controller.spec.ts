08cf8fd990ab5c3393c48d8e1aa7b91a1104826f
jeffmant <jgsmantovani@gmail.com>
2022-05-28 12:52:14 -0300

    test: ensure acronym controller return 201 if create a new one

--- src/domain/controllers/acronym.controller.spec.ts
+++ src/domain/controllers/acronym.controller.spec.ts
@@ -2,13 +2,24 @@ import { Sequelize } from 'sequelize-typescript'
 import request from 'supertest'
 import app from '../../app'
 import { AcronymModel } from '../../infra/database/sequelize/models/acronym.model'
+import { Acronym } from '../entity/acronym.entity'
+import { PaginateParams } from '../protocols/repository.interface'
 
 const server = app.listen()
 
 describe('Acronym Controller Unit Unit Tests', () => {
   let sequelize: Sequelize
+  let params: PaginateParams
 
   beforeEach(async () => {
+    params = {
+      filter: '',
+      orderParam: 'title',
+      orderBy: 'ASC',
+      offset: 0,
+      pageSize: 10
+    }
+
     sequelize = new Sequelize({
       dialect: 'sqlite',
       storage: ':memory:',
@@ -26,12 +37,39 @@ describe('Acronym Controller Unit Unit Tests', () => {
     server.close()
   })
 
-  it('Should returns 200 with paginated acronyms', async () => {
+  it('Should throw when acronym controller throws', async () => {
     try {
-      const response = await request(server).get('/acronyms').query({})
+      const response = await request(server).get('/acronyms').query(params)
+
+      expect(response.body).toBeUndefined()
+    } catch (error) {
+      expect(error).toBeDefined()
+    }
+  })
+
+  it('Should returns 200 and empty array result', async () => {
+    try {
+      const response = await request(server).get('/acronyms').query(params)
       expect(response.body).toBeDefined()
+      expect(response.statusCode).toBe(200)
       expect(response.body.total).toBe(0)
       expect(response.body.data).toEqual([])
+    } catch (error) {
+      expect(error).toBeUndefined()
+    }
+  })
+
+  it('Should returns 201 when create a acronym', async () => {
+    try {
+      const newAcronym = new Acronym('TDD', 'Test Driven Development')
+      const response = await request(server).post('/acronyms').send({
+        title: newAcronym.title,
+        definition: newAcronym.definition
+      })
+
+      expect(response.statusCode).toBe(201)
+      expect(response.body).toBeDefined()
+      expect(response.body).toEqual({ message: 'Acronym created' })
     } catch (error) {
       expect(error).toBeUndefined()
     }
