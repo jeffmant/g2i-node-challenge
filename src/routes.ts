8decadbdca1015351a0724f262360d6c0d55ea92
jeffmant <jgsmantovani@gmail.com>
2022-05-28 15:58:26 -0300

    feat: authentication middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import AcronymController from './domain/controllers/acronym.controller'
+import AuthMiddleware from './domain/middlewares/auth.middleware'
 
 class AppRouter {
   public router: Router
@@ -11,11 +12,11 @@ class AppRouter {
   }
 
   acronymRoutes (): void {
-    this.router.get('/acronyms', AcronymController.paginate)
-    this.router.get('/acronyms/:title', AcronymController.findOneByParam)
-    this.router.post('/acronyms', AcronymController.create)
-    this.router.put('/acronyms/:title', AcronymController.update)
-    this.router.delete('/acronyms/:title', AcronymController.delete)
+    this.router.get('/acronyms', AuthMiddleware, AcronymController.paginate)
+    this.router.get('/acronyms/:title', AuthMiddleware, AcronymController.findOneByParam)
+    this.router.post('/acronyms', AuthMiddleware, AcronymController.create)
+    this.router.put('/acronyms/:title', AuthMiddleware, AcronymController.update)
+    this.router.delete('/acronyms/:title', AuthMiddleware, AcronymController.delete)
   }
 }
 
